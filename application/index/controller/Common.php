7f7a7de6a9dd205e76039bc10bd42582644e06a9
xiaokethinkphp <425532268@qq.com>
2019-05-18 09:55:01 +0800

    建筑公司

---
+++ application/index/controller/Common.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+use \think\Controller;
+/**
+ *
+ */
+class Common extends Controller
+{
+
+    function __construct(argument)
+    {
+        // code...
+    }
+}
