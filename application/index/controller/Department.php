2f6f057fdb91f313a6ec2bdae5810771950c30a3
xiaokethinkphp <425532268@qq.com>
2019-05-21 17:04:39 +0800

    建筑公司

--- application/index/controller/Department.php
+++ application/index/controller/Department.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\index\controller;
 use \think\Controller;
-use  app\index\model\Department as DepartmentModel;
+// use  app\index\model\Department as DepartmentModel;
 /**
  * 单位管理类
  */
@@ -10,8 +10,11 @@ class Department extends Common
     // 人员列表方法
     public function lst()
     {
-        $title = "人员管理";
+        $title = "部门列表";
         $this->assign("title",$title);
+        $departmentModel = model("Department");
+        $departmentList = $departmentModel->getDepartmentList()->order("order");
+        $this->assign("departmentList",$departmentList);
         return view();
     }
 
@@ -24,7 +27,7 @@ class Department extends Common
             if (!$validate->check($post)) {
                 $this->error($validate->getError(),"index/department/lst");
             }
-            $departmentModel = new departmentModel();
+            $departmentModel = model("Department");
             $departmentAddResult = $departmentModel->addDepartmentInfo($post);
             if ($departmentAddResult == 0) {
                 $this->success("部门添加成功","index/department/lst");
@@ -32,9 +35,21 @@ class Department extends Common
                 $this->error("该部门已存在","index/department/lst");
             }
         } else {
-            $title = "添加人员";
+            $title = "添加部门";
             $this->assign("title",$title);
             return view();
         }
+    }
+
+    // 修改顺序的ajax方法
+    public function changeOrderAjax()
+    {
+        if (request()->isAjax()) {
+            $post = input("post.");
+            return $post;
+        } else {
+            $this->redirect("admin/department/lst");
+        }
+
     }
 }
