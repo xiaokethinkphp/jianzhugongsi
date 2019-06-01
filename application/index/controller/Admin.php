0835a13ef41ac8d0109c941674ca35ffac5d7a2a
xiaokethinkphp <425532268@qq.com>
2019-06-01 18:49:31 +0800

    建筑公司

--- application/index/controller/Admin.php
+++ application/index/controller/Admin.php
@@ -7,8 +7,29 @@ class Admin extends Common
 {
     public function upd($id='')
     {
-        if (request()->isPost) {
+        if (request()->isPost()) {
             $post = input("post.");
+            $adminFind = db('admin')->where([
+                'id'    =>  $post['id'],
+                'name'  =>  $post['name'],
+                'password'  =>  md5($post['old_password'])
+            ])->find();
+            if (!$adminFind) {
+                session("admin",null);
+                $this->error("密码错误，请重新登录","login/index");
+            }
+            $data = [
+                'password'  => md5($post['password'])
+            ];
+            $adminUpdResult = db("admin")
+            ->where("id",$post['id'])
+            ->data($data)->update();
+            if ($adminUpdResult!==false) {
+                $this->success("密码修改成功","index/index");
+            } else {
+                $this->error("密码修改失败","index/index");
+            }
+
         } else {
             $adminFind = db("admin")->find($id);
             if (!$adminFind) {
---
+++ application/index/validate/Admin.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\validate;
+use \think\Validate;
+/**
+ * 管理员验证器
+ */
+class Person extends Validate
+{
+    protected $rule = [
+        'name'  =>  "require|unique:person",
+        'total_vacation'    =>  "require|number|gt:1"
+    ];
+
+    protected $message = [
+        'name.require'  =>  "请输入人员名称",
+        'name.unique'   =>  "该人员已存在，请确认是否有重名情况。",
+        'total_vacation.require'    =>  "请输入假期天数",
+        'total_vacation.number'   =>  '请输入大于等于1的整数1',
+        'total_vacation.gt' =>  '请输入大于等于1的整数'
+    ];
+
+    protected $scene = [
+        'name'  =>  ['require','unique']
+    ];
+
+}
